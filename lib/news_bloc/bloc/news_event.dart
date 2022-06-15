4db866aa1b34d4b74087254fe4a2c2b24fdc989b
Pratul Pant <pratulpant02@gmail.com>
2022-06-16 02:39:38 +0530

    0.3

--- lib/news_bloc/bloc/news_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'news_bloc.dart';
-
-abstract class NewsEvent {}
-
-class getNewsEvent extends NewsEvent {}
-
-class openNewsEvent extends NewsEvent {}
--- lib/news_bloc/bloc/news_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'news_bloc.dart';
-
-abstract class NewsState {}
-
-class NewsInitial extends NewsState {}
-
-class NewsLoading extends NewsState {}
-
-class NewsLoaded extends NewsState {}
